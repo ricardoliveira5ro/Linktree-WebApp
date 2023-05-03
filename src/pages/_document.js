162b90d291e6a9c565afd24635d81ae0d699d945
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-05-03 20:54:33 +0100

    restore changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre&family=Open+Sans:wght@800&family=Shrikhand&family=Zen+Dots&display=swap" rel="stylesheet"></link>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
