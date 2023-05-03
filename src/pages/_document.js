534c36ecf94c36f3d31dda8945e1ab1427f2fa3c
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-05-03 21:14:21 +0100

    script tag update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,6 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre&family=Open+Sans:wght@800&family=Shrikhand&family=Zen+Dots&display=swap" rel="stylesheet"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
       </Head>
       <body>
         <Main />
