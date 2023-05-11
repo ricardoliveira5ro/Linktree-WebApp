956dceb7f050b32026286f332efbaaae49408000
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-05-11 17:58:32 +0100

    fix build

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -42,6 +42,7 @@ export default function Signup() {
         }
 
         // else successful
+        console.log('Success!')
         console.log(result)
         //return router.push("/admin")
     }
