1e5b3a713f8f3cd697ffe4abb08d43afdbb358cf
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-05-12 17:03:54 +0100

    signup validation

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre&family=Montserrat:wght@300&family=Open+Sans:wght@800&family=Shrikhand&family=Ubuntu:wght@500&family=Zen+Dots&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre&family=Montserrat:wght@300&family=Open+Sans:wght@800&family=Shrikhand&family=Ubuntu:wght@400;500&family=Zen+Dots&display=swap" rel="stylesheet"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
       </Head>
       <body>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -7,14 +7,16 @@ import Head from 'next/head'
 import firebase_app from '../../firebase-config'
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
-const auth = getAuth(firebase_app);
-
 export default function Signup() {
     const [windowHeight, setWindowHeight] = useState(null)
     const emailTemp = useRouter().query.emailTemp;
     const [inputValue, setInputValue] = useState(emailTemp);
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const router = useRouter();
+    const auth = getAuth(firebase_app);
 
     useEffect(() => {
         const handleResize = () => {
@@ -35,15 +37,122 @@ export default function Signup() {
     const handleForm = async (event) => {
         event.preventDefault()
 
+        var invalid = false;
+
+        resetInvalidMessages();
+
+        if (!validateEmail(email)) {
+            invalidEmail();
+            invalid = true;
+        }
+
+        if (password.length < 6) {
+            weakPassword()
+            invalid = true;
+        }
+
+        if (!matchPasswords(password, confirmPassword)) {
+            invalidMatchPasswords()
+            invalid = true;
+        }
+
+        if (!email) { document.getElementById('spanEmail').textContent = 'Required field' }
+
+        if (!password) {
+            emptyPassword()
+            invalid = true;
+        }
+        if (!confirmPassword) {
+            emptyConfirmPassword()
+            invalid = true;
+        }
+
+        if (invalid) { return }
+
         const { result, error } = await signUp(email, password);
 
         if (error) {
             return console.log(error)
         }
 
-        // else successful
-        console.log(result)
-        //return router.push("/admin")
+        return router.push('/')
+    }
+
+    const resetInvalidMessages = () => {
+        document.getElementById('email').style.border = 'none'
+        document.getElementById('spanEmail').style.display = 'none'
+
+        document.getElementById('password').style.border = 'none'
+        document.getElementById('spanPassword').style.display = 'none'
+
+        document.getElementById('confirmPassword').style.border = 'none'
+        document.getElementById('spanConfirmPassword').style.display = 'none'
+    }
+
+    const invalidEmail = () => {
+        document.getElementById('email').value = ''
+        document.getElementById('spanEmail').style.display = 'flex'
+        document.getElementById('email').style.border = '1px solid #dc2626'
+        document.getElementById('spanEmail').textContent = 'Invalid email format'
+    }
+
+    const weakPassword = () => {
+        document.getElementById('password').value = ''
+        document.getElementById('password').style.border = '1px solid #dc2626'
+        document.getElementById('spanPassword').style.display = 'flex'
+        document.getElementById('spanPassword').textContent = 'Passwords should be +6 characters'
+
+        document.getElementById('confirmPassword').value = ''
+        document.getElementById('confirmPassword').style.border = '1px solid #dc2626'
+        document.getElementById('spanConfirmPassword').style.display = 'flex'
+        document.getElementById('spanConfirmPassword').textContent = 'Passwords should be +6 characters'
+    }
+
+    const invalidMatchPasswords = () => {
+        document.getElementById('password').value = ''
+        document.getElementById('password').style.border = '1px solid #dc2626'
+        document.getElementById('spanPassword').style.display = 'flex'
+        document.getElementById('spanPassword').textContent = 'Passwords should match'
+
+        document.getElementById('confirmPassword').value = ''
+        document.getElementById('confirmPassword').style.border = '1px solid #dc2626'
+        document.getElementById('spanConfirmPassword').style.display = 'flex'
+        document.getElementById('spanConfirmPassword').textContent = 'Passwords should match'
+
+    }
+
+    const emptyPassword = () => {
+        document.getElementById('password').value = ''
+        document.getElementById('password').style.border = '1px solid #dc2626'
+        document.getElementById('spanPassword').style.display = 'flex'
+        document.getElementById('spanPassword').textContent = 'Required field'
+    }
+
+    const emptyConfirmPassword = () => {
+        document.getElementById('confirmPassword').value = ''
+        document.getElementById('spanConfirmPassword').style.display = 'flex'
+        document.getElementById('spanConfirmPassword').textContent = 'Required field'
+        document.getElementById('confirmPassword').style.border = '1px solid #dc2626'
+    }
+
+    const validateEmail = (email) => {
+        return email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    };
+
+    const matchPasswords = (password, confirmPassword) => {
+        return password === confirmPassword;
+    }
+
+    async function signUp(email, password) {
+        let result = null,
+            error = null;
+        try {
+            result = await createUserWithEmailAndPassword(auth, email, password);
+        } catch (e) {
+            error = e;
+        }
+    
+        return { result, error };
     }
 
     return (
@@ -55,12 +164,15 @@ export default function Signup() {
             </Head>
             <div className="main_signup_login" style={{ minHeight: windowHeight }}>
                 <div className="signup_login">
-                    <form className='form' onSubmit={handleForm}>
+                    <form className='form' id='form' onSubmit={handleForm}>
                         <h1>Create your account</h1>
                         <h3>One link, infinite connections.</h3>
-                        <input placeholder='Email' value={inputValue} onChange={handleInputChange} name="email" id="email" required type="email" autoComplete='off'></input>
-                        <input placeholder='Password' onChange={(e) => setPassword(e.target.value)} name="password" id="password" required type="password" autoComplete='off'></input>
-                        <input placeholder='Confirm Password' autoComplete='off' required type="password"></input>
+                        <input placeholder='Email' value={inputValue} onChange={handleInputChange} name="email" id="email" autoComplete='off'></input>
+                        <span id='spanEmail'>Invalid</span>
+                        <input placeholder='Password' onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password" autoComplete='off'></input>
+                        <span id='spanPassword'>Invalid</span>
+                        <input placeholder='Confirm Password' onChange={(e) => setConfirmPassword(e.target.value)} name="confirmPassword" id="confirmPassword" type="password" autoComplete='off'></input>
+                        <span id='spanConfirmPassword'>Invalid</span>
                         <button id='signup_btn' type="submit">Sign Up</button>
                         <p>Already have an account? <Link href='/login'>Log In</Link></p>
                     </form>
@@ -71,16 +183,4 @@ export default function Signup() {
             </div>
         </>
     )
-}
-
-async function signUp(email, password) {
-    let result = null,
-        error = null;
-    try {
-        result = await createUserWithEmailAndPassword(auth, email, password);
-    } catch (e) {
-        error = e;
-    }
-
-    return { result, error };
 }
